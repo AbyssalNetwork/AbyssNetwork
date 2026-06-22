dca66d40f1d5115cf4378f968eeffa018d937b0d
VardinsDev <vardins@vardinsdev.org>
2026-06-21 21:51:12 -0500

    Add: Give Command

---
+++ src/main/java/org/vardinsdev/abyssnetwork/commands/GiveCommand.java
@@ -0,0 +1,34 @@
+package org.vardinsdev.abyssnetwork.commands;
+
+import org.vardinsdev.minegun.Weapons.Rifle;
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.entity.Player;
+
+import org.vardinsdev.minegun.Weapons.RocketLauncher;
+import org.vardinsdev.minegun.minegunLogger;
+
+public class GiveCommand extends Command{
+
+    public GiveCommand() {
+        super("give");
+
+        var item = ArgumentType.Word("item-given")
+                .from("RocketLauncher", "Rifle"); // Auto Fill
+
+        addSyntax((sender, context) -> {
+            if (sender instanceof Player p) {
+                if (p.getPermissionLevel() >= 2) {
+                    if (context.get(item).equalsIgnoreCase("rocketLauncher")) {
+                        RocketLauncher.givePlayer(p);
+                    }
+                    if (context.get(item).equalsIgnoreCase("rifle")) {
+                        Rifle.givePlayer(p);
+                    }
+                }
+            } else {
+                minegunLogger.error("Console tried to give itself a rocket launcher!");
+            }
+        }, item);
+    }
+}
