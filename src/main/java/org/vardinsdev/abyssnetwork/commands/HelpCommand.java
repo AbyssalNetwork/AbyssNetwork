3451734f03d56618b3b652b12efc25d6bfe85648
aiden <aidensgreen049@gmail.com>
2026-07-12 20:55:02 -0400

    Edit: Text coloring for Help Add: Bolding for title Add: Spacing for text

--- src/main/java/org/vardinsdev/abyssnetwork/commands/HelpCommand.java
+++ src/main/java/org/vardinsdev/abyssnetwork/commands/HelpCommand.java
@@ -2,10 +2,12 @@ package org.vardinsdev.abyssnetwork.commands;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.entity.Player;
 
 import javax.swing.*;
+import javax.swing.text.StyledEditorKit;
 
 public class HelpCommand extends Command{
     public HelpCommand() {
@@ -13,11 +15,15 @@ public class HelpCommand extends Command{
 
         addSyntax((sender, context) -> {
             if(sender instanceof Player player) {
-                player.sendMessage(Component.text("|| HELP ||" +
-                        "  "
-                ));
-                player.sendMessage(Component.text("/give (weapon name) is used to provide yourself with the defined weapon.").color(NamedTextColor.RED));
-                player.sendMessage(Component.text("/test -- LOREM IPSUM").color(NamedTextColor.DARK_AQUA));
+                player.sendMessage(Component.text("|| AbyssNetwork: HELP ||").color(NamedTextColor.DARK_PURPLE).decorate(TextDecoration.BOLD));
+
+                player.sendMessage(""); // Spacer
+
+                player.sendMessage(Component.text("- give (weapon name) is used to provide yourself with the defined weapon.").color(NamedTextColor.AQUA));
+
+                player.sendMessage(""); // Spacer
+
+                player.sendMessage(Component.text("- test -- LOREM IPSUM").color(NamedTextColor.AQUA));
             }
         });
     }
