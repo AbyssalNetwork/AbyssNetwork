3b5467e97172255749bd75f99887ce2ec3d86c84
aiden <aidensgreen049@gmail.com>
2026-07-12 20:46:10 -0400

    Edit: Provided some color and a "temporary" banner for the help command.

--- src/main/java/org/vardinsdev/abyssnetwork/commands/HelpCommand.java
+++ src/main/java/org/vardinsdev/abyssnetwork/commands/HelpCommand.java
@@ -1,7 +1,11 @@
 package org.vardinsdev.abyssnetwork.commands;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.entity.Player;
+
+import javax.swing.*;
 
 public class HelpCommand extends Command{
     public HelpCommand() {
@@ -9,7 +13,11 @@ public class HelpCommand extends Command{
 
         addSyntax((sender, context) -> {
             if(sender instanceof Player player) {
-                player.sendMessage("/give (weapon name) is used to provide yourself with the defined weapon.");
+                player.sendMessage(Component.text("|| HELP ||" +
+                        "  "
+                ));
+                player.sendMessage(Component.text("/give (weapon name) is used to provide yourself with the defined weapon.").color(NamedTextColor.RED));
+                player.sendMessage(Component.text("/test -- LOREM IPSUM").color(NamedTextColor.DARK_AQUA));
             }
         });
     }
